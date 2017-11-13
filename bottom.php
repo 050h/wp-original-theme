5de366d8790591d13c76bc06743fc1d505b20f48
reizou05 <reizou05.0h@gmail.com>
2017-11-14 02:30:39 +0900

    add bottom widget

--- bottom.php
+++ bottom.php
@@ -1,31 +1,7 @@
 <section id="bottom">
 	<div class="content">
-		<div class="left-content">
-			<p class="box-title">PROFILE</p>
-			<img src="<?php echo get_template_directory_uri(); ?>/img/reizou05.jpg">
-			<p class="prof-content">
-				<a href="http://twitter.com/reizou05" target="_blank">@reizou05</a><br>
-				日本の高校生で趣味は筋トレ<br>
-				<a href="http://tento.tech" target="_blank">tento.tech</a>という学生プログラマー集団の長をやっています。<br>
-				WEB系のお仕事をください。
-			</p>
-		</div>
-		<div class="right-content">
-			<p class="box-title">CATEGORY</p>
-			<div class="categories">
-				<?php
-					$categories = get_categories(['hide_empty' => 0]);
-					if($categories){
-						foreach($categories as $category){
-							echo '<a href="'.get_category_link($category->term_id).'">'.$category->cat_name.'</a>';
-						}
-					}
-				?>
-			</div>
-		</div>
-		<div class="left-content">
-			<p class="box-title">AD</p>
-			<?php get_template_part('adlink'); ?>
+		<?php dynamic_sidebar('bottom-widget-left'); ?>
+		<?php dynamic_sidebar('bottom-widget-right'); ?>
 		</div>
 	</div>
 </section>
--- functions.php
+++ functions.php
@@ -1,4 +1,74 @@
 <?php
+
+	class Konyaku_category extends WP_Widget{
+		function __construct() {
+			parent::__construct(
+				'Konyaku_category', // Base ID
+				'Konyaku Category', // Name
+				array( 'description' => 'テーマ用のカテゴリーウィジェットです', ) // Args
+			);
+		}
+
+		public function widget( $args, $instance ) {
+			$title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Pages' ) : $instance['title'], $instance, $this->id_base);
+			echo $args['before_widget'];
+			echo '<p class="box-title">'.$title.'</p>';
+			echo '<div class="categories">';
+			$categories = get_categories(['hide_empty' => 0]);
+			if($categories){
+				foreach($categories as $category){
+					echo '<a href="'.get_category_link($category->term_id).'">'.$category->cat_name.'</a>';
+				}
+			}
+			echo '</div>';
+			echo $args['after_widget'];
+		}
+
+		public function form( $instance ){
+		$title = $instance['title'];
+		$title_name = $this->get_field_name('title');
+		$title_id = $this->get_field_id('title');
+		?>
+			<p>
+				<label for="<?php echo $title_id; ?>">タイトル:</label>
+				<input class="widefat" id="<?php echo $title_id; ?>" name="<?php echo $title_name; ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+			</p>
+		<?php
+		}
+
+		function update($new_instance, $old_instance) {
+			$instance = $old_instance;
+			$instance['title'] = strip_tags($new_instance['title']);
+
+			return $instance;
+		}
+	}
+
+	register_widget( 'Konyaku_category' );
+
+	// サイドバーウィジェット
+	register_sidebar(
+		array(
+			'name' => __( 'Bottom Widget Left' ),
+			'id' => 'bottom-widget-left',
+			'before_widget' => '<div class="left-content">',
+			'after_widget' => '</div>',
+			'before_title' => '<p class="box-title">',
+			'after_title' => '</p>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name' => __( 'Bottom Widget Right' ),
+			'id' => 'bottom-widget-right',
+			'before_widget' => '<div class="right-content">',
+			'after_widget' => '</div>',
+			'before_title' => '<p class="box-title">',
+			'after_title' => '</p>',
+		)
+	);
+
 	// アイキャッチ画像を有効にする。
 	add_theme_support('post-thumbnails');
 	// カテゴリ説明文にHTMLタグを使う
